30b14b7aaa18634f68765d06bd5d1eb205c78444
SantiagoSosssa <santiagosossa95@gmail.com>
2019-04-05 11:35:13 -0500

    feature - Angular proyect created, libraries installed, add nav bar and routing

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBkMinSzb4bWCBfhooaG7ymW71jgxa-Kyk",
+    authDomain: "online-shop-a23f4.firebaseapp.com",
+    databaseURL: "https://online-shop-a23f4.firebaseio.com",
+    projectId: "online-shop-a23f4",
+    storageBucket: "online-shop-a23f4.appspot.com",
+    messagingSenderId: "185908939767"
+  }
+};
